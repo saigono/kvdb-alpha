7df5bf4ae0f9f6c89bb58fa64da1adedfa6c2acd
Danil Ineev <danil@ineev.ru>
2025-07-14 16:23:06 +0100

    [DB-003] Build index on start, update on write

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,45 @@ use std::fs::OpenOptions;
 use std::io::{BufReader, BufWriter, Seek, stdin};
 use std::io::{SeekFrom, prelude::*};
 
-fn build_hash_table(file_path: &String) -> Result<HashMap<String, usize>, std::io::Error> {
+struct Environment {
+    file_path: String,
+    index: HashMap<String, u64>,
+}
+
+impl Environment {
+    pub fn new(file_path: &String) -> Self {
+        return Environment {
+            file_path: file_path.clone(),
+            index: build_index(file_path).unwrap(),
+        };
+    }
+}
+
+fn build_index(file_path: &String) -> Result<HashMap<String, u64>, std::io::Error> {
     let mut result = HashMap::new();
     let file = OpenOptions::new().read(true).open(file_path)?;
     let buf_reader = BufReader::new(file);
 
-    let mut current_position = 0;
+    let mut current_position: u64 = 0;
     for line in buf_reader.lines() {
         let real_line = line?;
-        println!("real line when building index: [{}]", real_line);
         let (line_key, _) = real_line
             .split_once(',')
             .expect("Failed to split line [{}].\nCheck for db corruption");
         result.insert(line_key.to_string(), current_position);
-        current_position += real_line.len() + 1; // accounting newline here
+        current_position += real_line.len() as u64 + 1; // accounting newline here
     }
     return Ok(result);
 }
 
 fn get_data(
-    file_path: &String,
-    key: &String,
-    index: HashMap<String, usize>,
+    env: &Environment,
+    key: &String
 ) -> Result<String, std::io::Error> {
-    let file = OpenOptions::new().read(true).open(file_path)?;
+    let file = OpenOptions::new().read(true).open(&env.file_path)?;
     let mut buf_reader = BufReader::new(file);
     let mut return_value = String::new();
-    match index.get(key) {
+    match env.index.get(key) {
         Some(offset) => {
             let _ = buf_reader.seek(SeekFrom::Start(*offset as u64));
             let mut real_line = String::new();
@@ -40,42 +52,34 @@ fn get_data(
                 .expect("Failed to split line [{}].\nCheck for db corruption");
             if line_key == key {
                 return_value = String::from(val);
+                return_value.pop(); // remove endline
+            } else {
+                panic!("index corrupted");
             }
         }
-        None => {
-            for line in buf_reader.lines() {
-                let real_line = line?;
-                println!("Current line: {}", real_line);
-                let (line_key, val) = real_line
-                    .split_once(',')
-                    .expect("Failed to split line [{}].\nCheck for db corruption");
-                if line_key == key {
-                    return_value = String::from(val);
-                }
-            }
-        }
+        None => ()
     };
     Ok(return_value)
 }
 
-fn set_data(file_path: &String, key: &String, value: &String) -> Result<(), std::io::Error> {
+fn set_data(env: &mut Environment, key: &String, value: &String) -> Result<(), std::io::Error> {
     let file = OpenOptions::new()
         .write(true)
         .append(true)
-        .open(file_path)?;
+        .open(&env.file_path)?;
     let mut writer = BufWriter::new(file);
-    writeln!(writer, "{},{}", key, value)?;
+    let line = format!("{},{}", key, value);
+    writeln!(writer, "{}", line)?;
+    env.index.insert(key.clone(), writer.stream_position()? - line.len() as u64 - 1);
     Ok(())
 }
 
-fn handle_command(command_args: &Vec<String>) {
-    let file_path = String::from("./test.db");
+fn handle_command(env: &mut Environment, command_args: &Vec<String>) {
     let command = &command_args[0];
     let key = &command_args[1];
-    let index = build_hash_table(&file_path).expect("Failed to build index");
     if command == "SET" {
         let value = &command_args[2];
-        let return_value = set_data(&file_path, key, value);
+        let return_value = set_data(env, key, value);
         match return_value {
             Ok(_) => {
                 println!("Written key: [{}] value: [{}]", key, value);
@@ -85,13 +89,13 @@ fn handle_command(command_args: &Vec<String>) {
             }
         }
     } else if command == "GET" {
-        let return_value = get_data(&file_path, key, index);
+        let return_value = get_data(env, key);
         match return_value {
             Ok(value) => {
                 if value.is_empty() {
                     println!("Value not found");
                 } else {
-                    println!("Found value: {}", value);
+                    println!("Found value: [{}]", value);
                 }
             }
             Err(e) => {
@@ -104,8 +108,9 @@ fn handle_command(command_args: &Vec<String>) {
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().skip(1).collect();
     let is_interactive = args[0] == "--interactive";
+    let mut env = Environment::new(&String::from("./test.db"));
     if !is_interactive {
-        handle_command(&args);
+        handle_command(& mut env, &args);
         return Ok(());
     }
     let stdin = stdin();
@@ -115,7 +120,7 @@ fn main() -> std::io::Result<()> {
                 print!("> ");
                 let command_args: Vec<String> =
                     real_line.splitn(3, ' ').map(|x| String::from(x)).collect();
-                handle_command(&command_args);
+                handle_command(&mut env, &command_args);
             }
             Err(e) => {
                 println!("Failed to work with DB, [{}]", e);
